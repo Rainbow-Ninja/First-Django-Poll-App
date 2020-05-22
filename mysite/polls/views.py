78b1064f851200d234df48d3280963d6a10b641b
Jo Dunham <jo@groklearning.com>
2020-05-22 12:54:41 +1000

    templates added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -30,16 +30,16 @@ class ResultsView(generic.DetailView):
   model = Question
   template_name = 'polls/results.html'
 
-  def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-      selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-      return render(request, 'polls/details.html', {
-        'question': question,
-        'error_message': "You didn't make a choice",
-      })
-    else:
-      selected_choice.votes += 1
-      selected_choice.save()
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+def vote(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/details.html', {
+      'question': question,
+      'error_message': "You didn't make a choice",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+  return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
